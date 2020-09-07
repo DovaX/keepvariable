44e6625e717aba6ee53f4a551fc5d9d504041cd8
Dominik Vach <vach.dominik@gmail.com>
2020-09-07 23:51:35 +0200

    example model

---
+++ keepvariable_model.py
@@ -0,0 +1,25 @@
+from keepvariable_core import Var,kept_variables,save_variables,load_variable
+
+
+a=Var("b")
+b=Var("c")
+
+c=a+b
+
+print(c)
+
+
+dict1=Var({1,2,3,4,5})
+
+list1=Var(a)
+
+print(kept_variables)
+save_variables(kept_variables)
+
+
+list1=load_variable()
+
+b=load_variable()
+
+c=Var(c)
+print(list1)
