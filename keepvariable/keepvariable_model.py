ada78a5e0d5d3e5c5a1271f649dc6413d88513b1
Dominik Vach <vach.dominik@gmail.com>
2020-12-31 22:37:11 +0100

    package structure of keep variable

--- keepvariable/keepvariable_model.py
+++ keepvariable/keepvariable_model.py
@@ -1,6 +1,5 @@
 from keepvariable_core import Var,kept_variables,save_variables,load_variable
 
-
 a=Var("b")
 b=Var("c")
 
@@ -8,14 +7,12 @@ c=a+b
 
 print(c)
 
-
 dict1=Var({1,2,3,4,5})
 
 list1=Var(a)
 
 print(kept_variables)
 save_variables(kept_variables)
-
 
 list1=load_variable()
 
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='keepvariable',
+    version='0.1.0',
+    author='DovaX',
+    author_email='dovax.ai@gmail.com',
+    description='A Python package keeping the values of variables between separate runs in a seamless and effortless way.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/DovaX/keepvariable',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          
+     ],
+    python_requires='>=3.6',
+)
+    
